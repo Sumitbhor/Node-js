0c9144c817f89f462f709478b65ec0882f9b05ae
Sumit Sudam Bhor <sumitbhor227@gmail.com>
2026-01-02 11:46:06 +0530

    basic node js tryout

---
+++ basics/hello.js
@@ -0,0 +1,18 @@
+console.log("🎉 Happy New Year 2026 🎉");
+var number = 239;
+
+function display() {
+    console.log("😊 Same to you!");
+}
+
+function display2() {
+    console.log("❓ How is your Node.js learning going?");
+}
+
+function display3() {
+    console.log("💪 I am learning Node.js!");
+}
+//multitasking
+setInterval(display, 3000);  
+setInterval(display2, 1000); 
+setInterval(display3, 500);  
