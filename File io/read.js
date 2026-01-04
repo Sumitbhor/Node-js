d8ec56c4b8e86726d0039057c716f06e5d90a5d9
Sumit Sudam Bhor <sumitbhor227@gmail.com>
2026-01-04 09:54:56 +0530

    simple file i/o opearations tryouts

---
+++ File io/read.js
@@ -0,0 +1,11 @@
+var fs = require ('fs');
+var fileName = "./data/info.txt";
+
+var onFileRead = function(err, data){
+    console.log("data from File available ");
+    console.log(data.toString());
+
+    
+};
+
+fs.readFile(fileName, onFileRead);
