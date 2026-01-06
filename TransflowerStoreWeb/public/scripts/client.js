67c5a52e0975d5c7648cf675eef96e65e7240e20
Sumit Sudam Bhor <sumitbhor227@gmail.com>
2026-01-06 17:31:37 +0530

    tryout's of dhtml pages

---
+++ TransflowerStoreWeb/public/scripts/client.js
@@ -0,0 +1,14 @@
+
+//app.js  is client side java script & server.js is server side java scripts 
+var count =23
+count++ ;
+console.log("this code is from app.js ")
+console.log("count"+ count);
+
+var onButtonClick= function(){
+    alert("here button is click..... ")
+}
+
+var onWelcomeClick= function(){
+    alert("Welcome to client side Event Handling ....")
+}
